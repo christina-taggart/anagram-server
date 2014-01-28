67763ddc2d321b2a9b7472cd187e4f438b4d4ade
Spencer Smitherman & Christina Taggart <pair+bigspencey.christina-taggart@devbootcamp.com>
2014-01-27 20:05:08 -0800

    Have anagram method implemented:

--- source/anagrams_1/app/controllers/index.rb
+++ source/anagrams_1/app/controllers/index.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 
-get '/:word' do
-  @word = params[:word]
+get '/' do
   erb :index
 end
+
+post '/:word' do
+  @word_to_anagram_check = params[:word_to_check]
+  @word = params[:word]
+  erb :anagrams
+end
+
+
--- source/anagrams_1/app/models/word.rb
+++ source/anagrams_1/app/models/word.rb
@@ -1,2 +1,28 @@
+require_relative '../../config/environment'
+require_relative '../../config/database'
+
 class Word < ActiveRecord::Base
+  def self.anagrams(test_word)
+    anagrams = []
+    Word.all.each do |word|
+      if word.text.split('').sort == test_word.split('').sort
+        anagrams << word
+      end
+    end
+    anagrams
+  end
 end
+
+p Word.anagrams("star")
+
+# class Word < ActiveRecord::Base
+#   def self.anagrams(word)
+#     anagrams = []
+#     Word.all.select do |test|
+#       if test.text.split("").sort.join == word.split("").sort.join
+#         anagrams << test
+#       end
+#     end
+#     p anagrams
+#   end
+# end

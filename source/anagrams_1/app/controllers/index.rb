9ec7dfe1b3f527021539af3efd76c268746a661a
Elliot Chalfant & Ricardo A. Rubio <pair+epchalfant.rickarubio@devbootcamp.com>
2014-01-27 18:23:46 -0800

    release 5: display anagrams feature completed

--- source/anagrams_1/app/controllers/index.rb
+++ source/anagrams_1/app/controllers/index.rb
@@ -1,5 +1,11 @@
 get '/:word' do
-  # Look in app/views/index.erb
   @word = params[:word]
+  @anagrams = check_for_anagrams(@word)
   erb :index
+end
+
+# returns an array of anagrams, or an empty array if none were found
+def check_for_anagrams(word)
+	word_to_check = Word.new(name: @word)
+	anagrams = word_to_check.anagrams
 end
--- source/anagrams_1/app/models/word.rb
+++ source/anagrams_1/app/models/word.rb
@@ -1,3 +1,22 @@
+require_relative '../../config/database.rb'
 class Word < ActiveRecord::Base
-  # Remember to create a migration!
+  # Returns an array of Word objects which represent anagrams of this word
+  # This can and should make calls to the db
+
+  # generate all letter combinations of base_word
+  # shove it into words_to_test
+  def anagrams
+  	base_word = self.name
+  	words_to_test = base_word.split('').permutation.to_a
+  	words_to_test.map! do |word|
+  		word.join('')
+  	end
+  	anagram_words = []
+  	# make calls to the DB to test these words
+  	words_to_test.each do |word|
+  		anagram_words << word unless Word.where(name: word).empty?
+	end
+  	# return the words that we found a match in the DB.
+  	anagram_words
+  end
 end

6fdc8380bae990184b58add313bc5144b9cfcf7d
Emmanuel Kaunitz & John Olmsted <pair+mechamanny.qsymmachus@devbootcamp.com>
2014-01-27 17:34:27 -0800

    Add #anagrams to Word, sinatra implementation of #anagrams

--- source/anagrams_1/app/controllers/index.rb
+++ source/anagrams_1/app/controllers/index.rb
@@ -1,9 +1,12 @@
 get '/:word' do
-  @word = params[:word]
+  word = Word.new(the_word: params[:word])
+  anagrams = word.anagrams.map { |word_object| word_object.the_word }
+  @anagrams = anagrams.to_s
+  @anagrams.gsub!(/[^a-zA-Z,]/, " ")
   erb :index
 end
 
 post '/get_anagram' do
   word = params[:user_input]
-  word.anagrams
+  redirect to("/#{word}")
 end
--- source/anagrams_1/app/models/word.rb
+++ source/anagrams_1/app/models/word.rb
@@ -1,9 +1,21 @@
+require_relative '../../config/environment'
+require_relative '../../config/database'
+
 class Word < ActiveRecord::Base
   def anagrams
-    Word.all
+    anagrams = words_of_equal_length.select { |word| is_anagram?(word.the_word, self.the_word) }
+    remove_duplicates(anagrams)
   end
 
   private
+
+  def words_of_equal_length
+    Word.all.select { |word| word.the_word.length == self.the_word.length }
+  end
+
+  def remove_duplicates(words)
+    words.select { |word| word.the_word.downcase != self.the_word.downcase }
+  end
 
   def canonical(string)
     string.downcase.split("").sort!
